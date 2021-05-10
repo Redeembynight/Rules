14eb0faa80f3594703ce8f30b6eebe7cf54591f9
Redeembynight <64399359+Redeembynight@users.noreply.github.com>
2021-05-10 14:37:43 +0800

    Update dayone.js

--- QuantumultX/script/unlock/dayone.js
+++ QuantumultX/script/unlock/dayone.js
@@ -4,14 +4,13 @@
 
 下载地址 https://apps.apple.com/us/app/day-one-journal/id1044867788
 
-^https:\/\/dayone\.me\/api\/(users|v2\/users\/account-status)$ url script-response-body https://raw.githubusercontent.com/langkhach270389/Scripting/master/dayone.js
+^https:\/\/dayone\.me\/api\/(users|v2\/users\/account-status)$ url script-response-body https://raw.githubusercontent.com/langkhach270389/Surge-LK/main/scripts/langkhach/dayone.js
 
 dayone.me, 
 */
-if ($response.statusCode == 200) {
-        if ($request.url.endsWith("account-status")) {
-
-            $done({body: JSON.stringify({
+if ($response.status == 200) {
+  if ($request.url.endsWith("account-status")) {
+$done({body: JSON.stringify({
   "expirationDate": 1893427199000,
   "startDate": 1546272000000,
   "subscriptionPlan": "com.bloombuilt.dayoneios.subscription.premium.yearly_discounted_trial",
@@ -20,13 +19,18 @@ if ($response.statusCode == 200) {
   "bundleReason": "purchase",
   "cancellationDate": 0
 } )});
-
-        } else {
-	     const bodyJson = JSON.parse($response.body);
-
-	     bodyJson.featureBundle = {"bundleName":"premium","features":[{"name":"imagesPerEntry","limit":30,"canUpgrade":false},{"name":"printingDiscount","canUpgrade":false},{"name":"syncMac","canUpgrade":false},{"name":"prioritySupport","canUpgrade":false},{"name":"sync","canUpgrade":false},{"name":"journalLimit","limit":100,"canUpgrade":false},{"name":"audioPerEntry","limit":10,"canUpgrade":false}]};
-
-	    $done({body: JSON.stringify(bodyJson)});
+} 
+else { let body= $response.body;
+	     let obj= JSON.parse(body);
+	     const feature= {"bundleName":"premium","features":[{"name":"imagesPerEntry","limit":30,"canUpgrade":false},{"name":"printingDiscount","canUpgrade":false},{"name":"syncMac","canUpgrade":false},{"name":"prioritySupport","canUpgrade":false},{"name":"sync","canUpgrade":false},{"name":"journalLimit","limit":100,"canUpgrade":false},{"name":"audioPerEntry","limit":10,"canUpgrade":false}]};
+    if(body.indexOf("featureBundle") !=-1)
+      {
+      obj["featureBundle"]= feature;
+      }
+       else {
+	    	obj["bundle"]= feature;
+	    }
+	    $done({body: JSON.stringify(obj)});
         }
 } else {
 	$done({});
