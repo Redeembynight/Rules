27af5e4107376a7ae67c33c5847a603ad22d3f6b
Redeembynight <64399359+Redeembynight@users.noreply.github.com>
2021-05-10 10:31:49 +0800

    camera

---
+++ QuantumultX/script/unlock/MolyCam_and_VaporCam.js
@@ -0,0 +1,12 @@
+/*
+Unlocks by Cuttlefish 公众号：墨鱼手记
+# MolyCam & VaporCam 拍出你的美解锁VIP
+下载地址：
+https://apps.apple.com/us/app/molycam-retro-effects-camera/id1304078556
+https://apps.apple.com/us/app/vaporcam-retro-filter-camera/id1246175190
+
+https:\/\/pay\.wecut\.com\/apple\/(iosVerifyReceipt|iosAppVerifyReceipt)\.php url script-response-body https://raw.githubusercontent.com/ddgksf2013/Cuttlefish/master/Crack/mv.js
+
+pay.wecut.com
+*/
+obj={"\x6d\x73\x67":"\u5904\u7406\u6210\u529f","\x64\x61\x74\x61":{"\x69\x73\x56\x61\x6c\x69\x64":1,"\x65\x78\x70\x69\x72\x65\x73\x54\x73":9876543210},"\x63\x6f\x64\x65":0};$done({body:JSON['\x73\x74\x72\x69\x6e\x67\x69\x66\x79'](obj)});
