d2dfa3ed728f1d71f5b64938c2ba869dbd48fad0
Redeembynight <64399359+Redeembynight@users.noreply.github.com>
2021-05-11 20:24:08 +0800

    sudoku and variations

---
+++ QuantumultX/script/unlock/ConceptisSudoku.js
@@ -0,0 +1,17 @@
+/*
+ConceptisSudoku解锁
+@787a68
+
+下载地址：https://apps.apple.com/us/app/conceptis-sudoku/id537201666
+
+[rewrite_local]
+^https?+:\/\/mobile\.conceptispuzzles\.com\/catalog_ios\/SudokuV2 url script-response-body https://raw.githubusercontent.com/787a68/Quantumult-X/main/Script/ConceptisSudoku.js
+
+mobile.conceptispuzzles.com
+*/
+
+var body = $response.body
+  .replace(/(<key>Free<\/key>\r\n\t\t\t\t<true \/>\r\n\t\t\t\t)/g, "")
+  .replace(/<key>Info<\/key>/g, "<key>Free</key>\r\n\t\t\t\t<true />\r\n\t\t\t\t<key>Info</key>");
+
+$done({ body });
