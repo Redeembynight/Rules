dd91cd787744c634c535ca56aaa87176827ffb92
Redeembynight <64399359+Redeembynight@users.noreply.github.com>
2021-05-23 17:56:48 +0800

    grammar and writing assistant

---
+++ QuantumultX/script/unlock/grammarly.js
@@ -0,0 +1,12 @@
+/*
+
+#grammarly（by 越南大佬）
+
+^https:\/\/subscription\.grammarly\.com\/api\/v1\/.* url script-response-body https://raw.githubusercontent.com/langkhach270389/Surge-LK/main/scripts/langkhach/grammarly.js
+
+subscription.grammarly.com
+*/
+
+let obj = JSON.parse($response.body)
+obj.isPremium = true;
+$done({body:JSON.stringify(obj)})
