12b90d0af70c7d028c46215ecf5ef3e13396548d
Redeembynight <64399359+Redeembynight@users.noreply.github.com>
2021-05-23 17:52:40 +0800

    Delete grammarly.js

--- QuantumultX/script/invalid/grammarly.js
+++
@@ -1,8 +0,0 @@
-/*
-#grammarly
-^https:\/\/subscription\.grammarly\.com\/api\/v1$ url script-response-body https://raw.githubusercontent.com/langkhach270389/Scripting/master/grammarly.js
-*.grammarly.com
-*/
-let obj = JSON.parse($response.body)
-obj.isPremium = true;
-$done({body:JSON.stringify(obj)})
