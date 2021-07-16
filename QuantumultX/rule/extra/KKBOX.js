422883041fb3790270b4cac6796feb523972b70c
Redeembynight <64399359+Redeembynight@users.noreply.github.com>
2021-07-16 11:12:12 +0800

    Update KKBOX.js

--- QuantumultX/rule/extra/KKBOX.js
+++ QuantumultX/rule/extra/KKBOX.js
@@ -1,27 +1,12 @@
-# NAME: KKBOX
-# AUTHOR: blackmatrix7
-# REPO: https://github.com/blackmatrix7/ios_rule_script
-# UPDATED: 2021-04-17 00:14:28
-# HOST-SUFFIX: 7
-# TOTAL: 7
-HOST-SUFFIX,kfs.io,KKBOX
-HOST-SUFFIX,kk.stream,KKBOX
-HOST-SUFFIX,kkbox.com,KKBOX
-HOST-SUFFIX,kkbox.com.tw,KKBOX
-HOST-SUFFIX,kktix.com,KKBOX
-HOST-SUFFIX,kktv.com.tw,KKBOX
-HOST-SUFFIX,kktv.me,KKBOX
+# > KKBOX
+USER-AGENT,*KKBOX,KKBOX&KKTV
+DOMAIN-SUFFIX,kkbox.com,KKBOX&KKTV
+DOMAIN-SUFFIX,kkbox.com.tw,KKBOX&KKTV
+DOMAIN-SUFFIX,kfs.io,KKBOX&KKTV
 
-# NAME: KKTV
-# AUTHOR: blackmatrix7
-# REPO: https://github.com/blackmatrix7/ios_rule_script
-# UPDATED: 2021-04-17 00:14:28
-# USER-AGENT: 2
-# HOST-SUFFIX: 2
-# HOST: 1
-# TOTAL: 5
-HOST,kktv-theater.kk.stream,KKTV
-HOST-SUFFIX,kktv.com.tw,KKTV
-HOST-SUFFIX,kktv.me,KKTV
-USER-AGENT,KKTV*,KKTV
-USER-AGENT,com.kktv.ios.kktv*,KKTV
+# > KKTV
+USER-AGENT,KKTV*,KKBOX&KKTV
+USER-AGENT,com.kktv*,KKBOX&KKTV
+DOMAIN-SUFFIX,kktv.me,KKBOX&KKTV
+DOMAIN-SUFFIX,kktv.com.tw,KKBOX&KKTV
+DOMAIN,kktv-theater.kk.stream,KKBOX&KKTV
