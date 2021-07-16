4bcfde14ada2d9d22e3d82825cb1a04425aee907
Redeembynight <64399359+Redeembynight@users.noreply.github.com>
2021-07-16 11:04:19 +0800

    Create KKBOX& KKTV.js

---
+++ QuantumultX/rule/extra/KKBOX& KKTV.js
@@ -0,0 +1,27 @@
+# NAME: KKBOX
+# AUTHOR: blackmatrix7
+# REPO: https://github.com/blackmatrix7/ios_rule_script
+# UPDATED: 2021-04-17 00:14:28
+# HOST-SUFFIX: 7
+# TOTAL: 7
+HOST-SUFFIX,kfs.io,KKBOX
+HOST-SUFFIX,kk.stream,KKBOX
+HOST-SUFFIX,kkbox.com,KKBOX
+HOST-SUFFIX,kkbox.com.tw,KKBOX
+HOST-SUFFIX,kktix.com,KKBOX
+HOST-SUFFIX,kktv.com.tw,KKBOX
+HOST-SUFFIX,kktv.me,KKBOX
+
+# NAME: KKTV
+# AUTHOR: blackmatrix7
+# REPO: https://github.com/blackmatrix7/ios_rule_script
+# UPDATED: 2021-04-17 00:14:28
+# USER-AGENT: 2
+# HOST-SUFFIX: 2
+# HOST: 1
+# TOTAL: 5
+HOST,kktv-theater.kk.stream,KKTV
+HOST-SUFFIX,kktv.com.tw,KKTV
+HOST-SUFFIX,kktv.me,KKTV
+USER-AGENT,KKTV*,KKTV
+USER-AGENT,com.kktv.ios.kktv*,KKTV
