9c96da8f6d6e6c94174212cae6de69cc6301ce15
Redeembynight <64399359+Redeembynight@users.noreply.github.com>
2021-07-30 13:47:29 +0800

    Update zn_cn.js

--- QuantumultX/rewrite/zn_cn.js
+++ QuantumultX/rewrite/zn_cn.js
@@ -1,5 +1,5 @@
-hostname = pay.nyxz166.com, zaihua.top
+hostname = pay.nyxz166.com,zaihua.top
 
 
 https://pay.nyxz166.com/ url request-header (\r\n)Accept-Language:.+(\r\n) request-header $1Accept-Language: zh-cn$2
-http://zaihua.top/ url request-header (\r\n)Accept-Language:.+(\r\n) request-header $1Accept-Language: zh-cn$2
+https://zaihua.top/ url request-header (\r\n)Accept-Language:.+(\r\n) request-header $1Accept-Language: zh-cn$2
