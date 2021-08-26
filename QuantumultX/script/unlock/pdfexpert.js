fc352485959ec1d0b0217a7bdf521d5875448e0d
Redeembynight <64399359+Redeembynight@users.noreply.github.com>
2021-08-27 02:18:07 +0800

    pdfexpert

---
+++ QuantumultX/script/unlock/pdfexpert.js
@@ -0,0 +1,29 @@
+/*
+Unlock Documents-PdfExpert-Scaner Pro（by 越南大佬）
+试用-取消订阅-恢复订阅
+
+[rewrite_local]
+^https:\/\/license\.pdfexpert\.com\/api\/.+\/subscription\/(refresh$|check$) url script-response-body https://raw.githubusercontent.com/langkhach270389/Surge-LK/main/scripts/langkhach/pdfexpert.js
+
+[mitm]
+hostname = license.pdfexpert.com
+*/
+
+re('\\w{1,2}\/\\w{1,2}\/\\w{4}@"subscriptionState":"\\w+"@"is(.+)User":false','25/11/2099@"subscriptionState":"active"@"is$1User":true');
+
+function re() {
+var body = $response.body;
+ if (arguments[0].includes("@")) {
+  var regs = arguments[0].split("@");
+  var strs = arguments[1].split("@");
+  for (i = 0;i < regs.length;i++) {
+   var reg = new RegExp(regs[i],"g");
+   body = body.replace(reg, strs[i]);
+ }
+}
+ else {
+  var reg = new RegExp(arguments[0],"g");
+  body = body.replace(reg, arguments[1]);
+}
+ $done({body});
+} 
